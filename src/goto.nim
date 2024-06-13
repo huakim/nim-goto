e53fa81bb763e00f3ba6c89341c237cd4caff4d2
huakim <realkig@gmail.com>
2024-06-13 20:50:26 +0300

    update

--- src/goto.nim
+++ src/goto.nim
@@ -2,16 +2,29 @@ import std/macros
 
 macro label*(labelName, body: untyped): untyped =
   expectKind(labelName, nnkIdent)
-  let name = repr(labelName)
+  let name = "NIMROD_GOTO_LABEL_" & repr(labelName)
   result = quote do:
     {.emit: `name` & ":".}
     `body`
 
+macro label*(labelName: untyped): untyped =
+  expectKind(labelName, nnkIdent)
+  let name = "NIMROD_GOTO_LABEL_" & repr(labelName)
+  result = quote do:
+    {.emit: `name` & ":".}
+
 macro goto*(labelName: untyped): untyped =
   expectKind(labelName, nnkIdent)
-  let name = repr(labelName)
+  let name = "NIMROD_GOTO_LABEL_" & repr(labelName)
   result = quote do:
     {.emit: "goto " & `name` & ";".}
 
 template jmp*(labelName: untyped): untyped =
   goto labelName
+
+macro until*(condition, body: untyped): untyped =
+  result = quote do:
+    `body`
+    while `condition`:
+      `body`
+
